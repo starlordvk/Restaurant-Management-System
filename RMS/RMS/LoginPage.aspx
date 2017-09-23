90bacb5adc5147d27a10af0ab95c6e405378d656
Gaurav Kudva <gauravkudva2@gmail.com>
2017-09-23 20:34:15 +0530

    Added RMS Folder (Complete Web App solution including Login Page and Home Page (Blank)

---
+++ RMS/RMS/LoginPage.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="RMS.LoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!-- Label and TextBox for Username -->
+            <asp:Label runat="server" Text="Username: "></asp:Label>
+            <asp:TextBox runat="server" ID="username_tb"></asp:TextBox>
+            <br /><br />
+            <!-- Label and TextBox for Password-->
+            <asp:Label runat="server" Text="Password: "></asp:Label>
+            <asp:TextBox runat="server" ID="password_tb" TextMode="Password"></asp:TextBox>
+            <br /><br />
+            <!-- Login Button -->
+            <asp:Button runat="server" ID="login_button" Text="Login" OnClick="ShowHomePage" />
+            <br /><br />
+            <!-- Validators for ensuring the Username and Password Fields are not empty -->
+            <asp:RequiredFieldValidator runat="server" ID="rfv_username" ControlToValidate="username_tb" ForeColor="Red" ErrorMessage="Username is mandatory" Display="None"></asp:RequiredFieldValidator>
+            <asp:RequiredFieldValidator runat="server" ID="rfv_password" ControlToValidate="password_tb" ForeColor="Red" ErrorMessage="Password is mandatory" Display="None"></asp:RequiredFieldValidator>
+            <!-- Validators for ensuring the Username and Password are valid -->
+            <asp:CompareValidator runat="server" ID="compv_username" ControlToValidate="username_tb" ValueToCompare="oriental_admin" Operator="Equal" Type="String" ErrorMessage="Invalid Username" ForeColor="Red" Display="None"></asp:CompareValidator>
+            <asp:CompareValidator runat="server" ID="compv_password" ControlToValidate="password_tb" ValueToCompare="@dMin123" Operator="Equal" Type="String" ErrorMessage="Invalid Password" ForeColor="Red" Display="None"></asp:CompareValidator>
+            <!-- Summary of Validations required -->
+            <asp:ValidationSummary runat="server" ShowSummary="true" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RMS/RMS/LoginPage.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RMS
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Function to Handle Button Click
+        protected void ShowHomePage(object sender, EventArgs e)
+        {
+            //Transfers to Home Page if all the Login Fields are valid
+            if(Page.IsValid)
+            {
+                Response.Redirect("HomePage.aspx");
+            }
+        }
+
+        //Custom Password Validation Function
+        /*protected void ValidatePassword(object sender, ServerValidateEventArgs e)
+        {
+            string password = e.Value;
+            if(password.Length >= 6)
+            {
+                int digit_flag = 0, cap_flag = 0, specchar_flag = 0, validate_flag = 0;
+                for(int x = 0; x < password.Length; x++)
+                {
+                    if(password[x] == '@' || password[x] == '_')
+                    {
+                        specchar_flag = 1;
+                    }
+                    if(password[x] >= 'A' && password[x] <= 'Z')
+                    {
+                        cap_flag = 1;
+                    }
+                    if(password[x] >= '0' && password[x] <= '9')
+                    {
+                        digit_flag = 1;
+                    }
+                    if(digit_flag == 1 && cap_flag == 1 && specchar_flag == 1)
+                    {
+                        validate_flag = 1;
+                        break;
+                    }
+                }
+
+                if(validate_flag == 1)
+                {
+                    e.IsValid = true;
+                }
+                else
+                {
+                    e.IsValid = false;
+                    cv_password.ErrorMessage = "Password is missing a special character, digit, or an uppercase letter";
+                }
+            }
+            else
+            {
+                e.IsValid = false;
+                cv_password.ErrorMessage = "Password Length should be minimum of 6 characters";
+            }
+        }*/
+    }
+}
